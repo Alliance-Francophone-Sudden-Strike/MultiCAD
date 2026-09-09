7d3d37b9b2a37924b39c48e4f59cfde3acb2ed1c
Jean Tinland <jean.tinland@gmail.com>
2026-09-09 17:49:20 +0200

    fix: implement multiplayer zepellin fix + hide zoom indicator with alt+y

--- src/patcher/GameDllHooks.h
+++ src/patcher/GameDllHooks.h
@@ -1106,6 +1106,7 @@ private:
         void(__thiscall* drawHorLine)(UiElementBase*, int, int, int, int16_t);
         void(__thiscall* drawVertLine)(UiElementBase*, int, int, int, int16_t);
         void(__stdcall* drawPlaneCrosses)(int, int, int);
+        uintptr_t* strategicMapOverlay;
 
         UnitData* units;
         int unitVtableOffset;
